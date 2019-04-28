8b312acca689493cd7827b82271e811b5fd3a1c3
garuhester <garuhester@gmail.com>
2019-04-28 11:13:43 +0800

    u

--- testing-files/demo1.air/demo1.py
+++ testing-files/demo1.air/demo1.py
@@ -7,5 +7,14 @@ auto_setup(__file__)
 
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
-
-poco(text="计算器").click()
+#start
+while True:
+    try:
+        poco(text="计算器").click()
+        break
+    except:
+        print('发生了异常')
+        home()
+    finally:
+        print('最后执行')
+#end
--- testing-files/demo2.air/demo2.py
+++ testing-files/demo2.air/demo2.py
@@ -7,5 +7,14 @@ auto_setup(__file__)
 
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
-
-poco(text="日历").click()
+#start
+while True:
+    try:
+        poco(text="日历").click()
+        break
+    except:
+        print('发生了异常')
+        home()
+    finally:
+        print('最后执行')
+#end
